5539a9a1e4820a993e02f572b1a250ac55b9cb91
matjaz <matjaz@matjazpirnovar.com>
2015-02-14 19:08:43 -0800

    xml_well_formedness_check program

---
+++ xml_well_formdness_check/test/test_checker1.py
@@ -0,0 +1,36 @@
+'''
+Test cases to test checker1 module.
+'''
+
+
+from xml_well_formdness_check.checker1 import *
+
+
+#file = '/home/matjaz/PycharmProjects/xml_well_formedness_check/xml_well_formdness_check/xml_example.txt'
+
+def test_get_string():
+    assert getstring() != None
+
+
+def test_root_element_opening():
+    pass
+
+def test_root_element_closing():
+    pass
+
+def test_root_element_exists_and_matches():
+    pass
+
+def test_number_of_angle_brackets_is_even():
+    pass
+
+def test_get_all_tags_in_order():
+    pass
+
+def test_each_element_has_strictly_two_identical_names_for_each_tag():
+    #assert each_element_has_strictly_two_identical_names_for_each_tag()
+    pass
+
+
+
+
